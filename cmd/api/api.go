4b87a173591b036eecf908e36a999b35d17d99d8
Rizky Fahrureza <108663566+mzzqq@users.noreply.github.com>
2024-07-17 14:38:46 +0700

    feat(api): add APIServer with basic setup and routing
    - Introduced APIServer struct with address and database fields.
    - Added NewAPIServer function to initialize the server.
    - Implemented Start method to set up routing with Gorilla Mux and start the HTTP server.

---
+++ cmd/api/api.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"database/sql"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+type APIServer struct {
+	addr string
+	db   *sql.DB
+}
+
+func NewAPIServer(addr string, db *sql.DB) *APIServer {
+	return &APIServer{
+		addr: addr,
+		db:   db,
+	}
+}
+
+func (s *APIServer) Start() error {
+	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+	log.Println(("Listening: ", s.addr))
+	return http.ListenAndServe(s.addr, router)
+}
